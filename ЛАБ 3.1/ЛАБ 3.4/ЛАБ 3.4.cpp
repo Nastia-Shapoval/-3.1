1e3864559a87f1c5596fcb745e8a9b8c92e4e712
Nastia-Shapoval <148361310+Nastia-Shapoval@users.noreply.github.com>
2025-04-09 11:08:22 +0300

    Add files via upload

---
+++ ЛАБ 3.1/ЛАБ 3.4/ЛАБ 3.4.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter an integer: ");
+    scanf_s("%d", &num);
+
+    if (num % 2 == 0) {
+        printf("The number is even\n");
+    }
+    else {
+        printf("The number is odd\n");
+    }
+
+    return 0;
+}
+
