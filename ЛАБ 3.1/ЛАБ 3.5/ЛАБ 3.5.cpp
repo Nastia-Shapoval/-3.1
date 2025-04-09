7eca5ee458fec3d941dd94abaebadccdcff78c2c
Nastia-Shapoval <148361310+Nastia-Shapoval@users.noreply.github.com>
2025-04-09 11:09:14 +0300

    Add files via upload

---
+++ ЛАБ 3.1/ЛАБ 3.5/ЛАБ 3.5.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char ch;
+    printf("Enter a lowercase English letter: ");
+    scanf_s(" %c", &ch);  
+
+    if (ch >= 'a' && ch <= 'z') {
+        char upper = toupper(ch);
+        printf("Uppercase letter: %c\n", upper);
+    }
+    else {
+        printf("This is not a lowercase English letter\n");
+    }
+
+    return 0;
+}
