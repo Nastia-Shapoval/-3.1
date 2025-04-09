7f3287a9dbc099c831b9e496d6e477c590caddc8
Nastia-Shapoval <148361310+Nastia-Shapoval@users.noreply.github.com>
2025-04-09 11:07:27 +0300

    Add files via upload

---
+++ ЛАБ 3.1/ЛАБ 3.2/ЛАБ 3.2.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float r;
+    printf("Enter the radius of the circle: ");
+    scanf_s("%f", &r);
+
+    const float PI = 3.14159265;
+    float area = PI * r * r;
+    printf("Area of the circle: %.2f\n", area);
+
+    return 0;
+}
