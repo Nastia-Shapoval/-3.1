60b9aa132c9e89814fae7fd240c4389ecc97d275
Nastia-Shapoval <148361310+Nastia-Shapoval@users.noreply.github.com>
2025-04-09 11:07:57 +0300

    Add files via upload

---
+++ ЛАБ 3.1/ЛАБ 3.3/ЛАБ 3.3/ЛАБ 3.3.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main() {
+    int seconds;
+    printf("Enter the number of seconds: ");
+    scanf_s("%d", &seconds);
+
+    int h = seconds / 3600;
+    int m = (seconds % 3600) / 60;
+    int s = seconds % 60;
+
+    printf("Hours: %d, Minutes: %d, Seconds: %d\n", h, m, s);
+
+    return 0;
+}
