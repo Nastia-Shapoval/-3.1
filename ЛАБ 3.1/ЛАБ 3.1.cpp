95d472112646be4b446523708cf9a9a1f6c55b22
Nastia-Shapoval <148361310+Nastia-Shapoval@users.noreply.github.com>
2025-04-09 11:06:45 +0300

    Add files via upload

---
+++ ЛАБ 3.1/ЛАБ 3.1.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main() {
+    float meters;
+    printf("Enter length in meters: ");
+    scanf_s("%f", &meters);
+
+    printf("Centimeters: %.2f cm\n", meters * 100);
+    printf("Millimeters: %.2f mm\n", meters * 1000);
+
+    return 0;
+}
